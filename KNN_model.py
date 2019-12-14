cff6209edd1902ffddd6bb9b9639967107334080
Taha Tauquir <Taha24888@gmail.com>
2019-12-15 00:22:53 +0500

    v1.5.6
    :small_blue_diamond:  KNN Model Class added!

---
+++ KNN_model.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+
+class KNN_Model():
+    classifier = KNeighborsClassifier(n_neighbors=5)
+    scaler = StandardScaler()
+        
+    
+    def train_model(self):
+         
+         names = ['Pulse', 'GSR','Emotions']
+         dataset = pd.read_csv("C:/Users/Zeeshan/dec.csv",names=names)
+         print(dataset)
+         X = dataset.iloc[:, :-1].values
+         y = dataset.iloc[:, 2].values
+        
+         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
+         
+         scaler.fit(X_train)
+    
+         X_train = scaler.transform(X_train)
+         X_test = scaler.transform(X_test)
+        
+         classifier.fit(X_train, y_train)
+        
+         test=scaler.transform(X_test)
+         y_pred = classifier.predict(test)
+         print(confusion_matrix(y_test, y_pred))
+         print(classification_report(y_test, y_pred))
+         
+        
+    def test_model(self,testSet):
+         p=scaler.transform(testSet)
+         k_pred = classifier.predict(p)
+         return k_pred
+    
+
+x=KNN_Model();
+x.train_model()   
+print(x.test_model( [[8 ,200],[7.5,300 ], [3.6,100]]) )
+print(x.test_model( [[3.2 ,100],[7.5,300 ], [9.6,200]]) )
+     
+             
+        
+        
+        
